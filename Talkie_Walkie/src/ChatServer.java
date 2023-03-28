a25f2e4c2ac63e403a3fb15320d32d95969afd55
Tristan <turgytristan@gmail.com>
2023-03-28 09:58:25 +0200

    chat seveur on

--- Talkie_Walkie/src/ChatServer.java
+++ Talkie_Walkie/src/ChatServer.java
@@ -7,14 +7,15 @@ public class ChatServer {
     private static List<ClientHandler> clients = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(PORT);
-        System.out.println("Chat Server démarré sur le port: " + PORT);
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Chat Server démarré sur le port: " + PORT);
 
-        while (true) {
-            Socket clientSocket = serverSocket.accept();
-            ClientHandler client = new ClientHandler(clientSocket);
-            clients.add(client);
-            new Thread(client).start();
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                ClientHandler client = new ClientHandler(clientSocket);
+                clients.add(client);
+                new Thread(client).start();
+            }
         }
     }
 
